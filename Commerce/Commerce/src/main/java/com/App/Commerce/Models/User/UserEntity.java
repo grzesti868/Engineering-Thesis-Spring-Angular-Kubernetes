3457530dae7e7a92347ca067487c8c394ce5ecda
grzesti868 <gregustich@gmail.com>
2022-02-26 20:47:15 +0100

    added table, modify models

--- Commerce/Commerce/src/main/java/com/App/Commerce/Models/User/UserEntity.java
+++ Commerce/Commerce/src/main/java/com/App/Commerce/Models/User/UserEntity.java
@@ -3,16 +3,25 @@ package com.App.Commerce.Models.User;
 
 import com.App.Commerce.Enums.StatusEnum;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 
+@Entity
+@Table(name = "users")
 public class UserEntity {
 
+    @Id
+    @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 25)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
     private Long id;
     private String username;
     private String password;
